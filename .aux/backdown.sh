3fdb34ff9cd3c667e1527dc64bdcbb3064858bd4
Liemani <parkjh2768@naver.com>
2021-02-05 10:37:41 +0900

    backup

--- .aux/backdown.sh
+++ .aux/backdown.sh
@@ -3,7 +3,16 @@
 source $pathvar
 source $env/gitdata.sh
 
+if [[ -d $HOME/.ssh ]]; then
+	git_address=git@github.com:
+else
+	git_address=https://github.com/
+fi
+
 for ((i = $#gitlist; i > 0; i--))
 	gitlist[i]=$git/$gitlist[i]
+	if [[ ! -d $git/$gitlist[i] ]]; then
+		git clone $git_address$git_user_id/$repository.git $git/$gitlist[i]
+	fi
 
 zsh $aux/gitpull.sh $gitlist
