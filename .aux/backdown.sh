d62798a5483960efa8c15bc55ec42235723b22f5
Liemani <parkjh2768@naver.com>
2021-02-05 10:41:40 +0900

    backup

--- .aux/backdown.sh
+++ .aux/backdown.sh
@@ -9,10 +9,12 @@ else
 	git_address=https://github.com/
 fi
 
-for ((i = $#gitlist; i > 0; i--))
+for ((i = $#gitlist; i > 0; i--)); do
 	gitlist[i]=$git/$gitlist[i]
-	if [[ ! -d $git/$gitlist[i] ]]; then
-		git clone $git_address$git_user_id/$repository.git $git/$gitlist[i]
+	if [[ ! -d $gitlist[i] ]]; then
+		git clone $git_address$git_user_id/$repository.git $gitlist[i]
+		gitlist[i]=""
 	fi
+done
 
 zsh $aux/gitpull.sh $gitlist
