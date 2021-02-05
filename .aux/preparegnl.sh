58ef60e5efa790c57e35578eab5f12409d7e9bdc
Liemani <parkjh2768@naver.com>
2021-02-05 14:36:00 +0900

    backup

--- .aux/preparegnl.sh
+++ .aux/preparegnl.sh
@@ -10,8 +10,11 @@ git clone https://github.com/mrjvs/42cursus_gnl_tests.git "$dest_path/42cursus_g
 git clone https://github.com/C4r4c0l3/gnl-war-machine-v2019.git "$dest_path/gnl-war-machine-v2019"
 git clone https://github.com/Sherchryst/gnlkiller.git "$dest_path/gnlkiller"
 git clone https://github.com/DontBreakAlex/gnlkiller.git "$dest_path/gnlkiller_Alex"
+git clone https://github.com/Sherchryst/gnlkiller.git "$dest_path/gnlkiller2"
 git clone https://github.com/charMstr/GNL_lover.git "$dest_path/gnl_lover"
 git clone https://github.com/harm-smits/gnl-unit-test.git "$dest_path/gnl-unit-test"
 git clone https://github.com/Mazoise/42TESTERS-GNL.git "$dest_path/42TESTERS-GNL"
+git clone https://github.com/PandaCoustik/Test-42.git "$dest_path/Test-42.git"
+git clone https://github.com/Tripouille/gnlTester.git "$dest_path/gnlTester"
 
 unset dest_path
