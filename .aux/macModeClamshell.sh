52e74f8a1bb67c9451e8890c6936f54193b40f8e
Liemani <parkjh2768@naver.com>
2022-01-27 23:40:48 +0900

    Read C++ Primer Plus 5ed 256 ~ 274

---
+++ .aux/macModeClamshell.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+sudo pmset -b sleep 0
+sudo pmset -b hibernatemode 0
+sudo pmset -b disablesleep 1
---
+++ .aux/macModeOrigin.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+sudo pmset -b sleep 1
+sudo pmset -b hibernatemode 3
+sudo pmset -b disablesleep 0
