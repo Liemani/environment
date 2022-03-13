e682fad89a4c89288f6fbe64b66a0edbf0cd6c08
Liemani <parkjh2768@naver.com>
2022-03-13 22:52:04 +0900

    Finish cppModule02

--- .aux/git/amend.sh
+++ .aux/git/amend.sh
@@ -1,9 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 
-if [[ $# -ne 0 ]]; then
-	echo 'usage: aux commit'
+if [[ $# -ge 2 ]]; then
+	echo 'usage: aux amend [<commit message>]'
 	exit 0
 fi
 
-git add --all \
-	&& git commit --amend --no-edit
+if [[ $# -eq 0 ]]; then
+	git add --all \
+		&& git commit --amend --no-edit
+elif [[ $# -eq 1 ]]; then
+	git add --all \
+		&& git commit --amend -m "$1"
+fi
