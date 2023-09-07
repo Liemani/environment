f7a2346e914809ea65e3f099021a3ab88ebc8bb8
Liemani <parkjh2768@gmail.com>
2023-09-07 10:59:08 +0900

    Create clang++_x.sh

--- .aux/encode/clang_x.sh
+++ .aux/encode/clang_x.sh
@@ -3,8 +3,11 @@
 # generate executable program for x86_64
 
 if [[ $# -eq 0 ]]; then
-	echo 'usage: aux clang_x [option] <filename> ...'
+	echo 'usage: aux clang_x [option] <file> ...'
     exit 1
 fi
 
-clang -O1 -target x86_64-apple-macos10.12 $@
+filename=${1%.*}
+
+# clang -O1 -target x86_64-apple-macos10.12 -o $filename $@
+clang -target x86_64-apple-macos10.12 -o $filename $@
