1e59061acd9992e1046d3b0cc67c15ae60942f08
Liemani <parkjh2768@gmail.com>
2023-09-18 00:01:46 +0900

    Fix bin/_vim

--- script/sh/vim/_vim.sh
+++ script/sh/vim/_vim.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-oldpwd=$PWD
-
-
-
-if [ $# -eq 1 ]; then
-    cd $(dirname $1)
-    set -- "$(basename $1)"
-elif [ "$1" = '-S' ]; then
-    cd $(dirname $2)
-    set -- '-S' "$(basename $2)"
+if [ "$1" = '-S' ]; then
+    cd $(dirname "$2")
+else
+    cd $(dirname "$1")
 fi
 
 export git_root=$(2>/dev/null git rev-parse --show-toplevel)
 export git_repo=$(2>/dev/null basename $git_root)
 export git_branch=$(2>/dev/null git rev-parse --abbrev-ref HEAD)
 
+cd -
+
 command vim "$@"
 
 
 
-cd $oldpwd
-
 unset git_root
 unset git_repo
 unset git_branch
-unset oldpwd
