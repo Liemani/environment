5775b1ebc116b36c63e57eead2568190efbf63dc
Liemani <parkjh2768@gmail.com>
2023-09-19 09:38:39 +0000

    Create bin/mv

--- script/sh/foundation/ln.sh
+++ script/sh/foundation/ln.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-    echo 'usage: ln <target>'
+    echo 'usage : ln <target>'
     echo '    target is relative path from $env/script/sh'
     return 1
 fi
 
+target_absolute=$env/script/sh/$1
+
+if [ ! -f "$target_absolute" ]; then
+    echo 'a::ln : no file exist ['"$target_absolute"']'
+    unset target_absolute
+    return 1
+else
+    unset target_absolute
+fi
+
 basename=$(basename $1)
 filename=${basename%.*}
-target=../script/sh/$1
+target_relative=../script/sh/$1
 
-if [ -f $target ]; then
-    ln -s $target $env/bin/$filename
-else
-# fixme: output absolute path
-    echo 'ln: no file ['"$target"']'
-fi
+ln -s "$target_relative" "$env/bin/$filename"
 
 unset basename
 unset filename
-unset target
+unset target_relative
--- script/sh/foundation/mv.sh
+++ script/sh/foundation/mv.sh
@@ -1,9 +1,45 @@
 #!/bin/sh
 
 if [ $# -ne 2 ]; then
-    echo 'mv'
+    echo 'usage : mv <source> <target>'
+    echo '    source and target are relative path from $env/script/sh'
     return 1
 fi
 
-mv $env/script/sh/$1 $env/script/sh/$2
-#ln
+source_absolute=$env/script/sh/$1
+
+if [ ! -f "$source_absolute" ]; then
+    echo 'a::mv : no file exist ['"$source_absolute"']'
+    unset source_absolute
+    return 1
+else
+    unset source_absolute
+fi
+
+if [ "$1" = "$2" ]; then
+    echo 'source and target files are same'
+    return 1
+fi
+
+mv "$env/script/sh/$1" "$env/script/sh/$2"
+
+source_basename=$(basename $1)
+source_filename=${source_basename%.*}
+
+target_basename=$(basename $2)
+target_filename=${target_basename%.*}
+
+rm "$env/bin/$source_filename"
+
+if [ "$source_filename" = "$target_filename" ]; then
+    option='-s'
+else
+    option='-sf'
+fi
+
+ln "$option" "../script/sh/$2" "$env/bin/$target_filename"
+
+unset source_basename
+unset source_filename
+unset target_basename
+unset target_filename
