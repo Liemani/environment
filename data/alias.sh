4ec962f4e4f8b7f3eaee77a3caf8ad51c64f1de3
Liemani <parkjh2768@gmail.com>
2023-09-20 13:41:56 +0900

    Update data/alias.sh

--- data/alias.sh
+++ data/alias.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# general command
 alias ls='ls -Ap1'
 alias mv='mv -i'	# prevent silent overwriting
 alias cp='cp -i'	# prevent silent overwriting
@@ -9,21 +10,24 @@ alias cdgitroot='cd $(git rev-parse --show-toplevel)'
 alias cdtoday='cd $local/.temp && mkdir $(today); cd $(today)'
 alias gitlog='git log --all --graph --max-count=42 --oneline'
 alias gitlogall='git log --all --graph --oneline'
-alias pour='a pour'
-alias record='a record'
-alias vs='a vimsession'
 alias gitd='git status && gitlog'
 
-## script aliases
+# a::command
 alias a='source $env/bin/a'
+alias dump='a dump'
+alias record='a record'
 alias vim='a _vim'
+# alias vs='a vimsession'
+
+## script
 alias shrc='source $HOME/.'"$shell"'rc'
 
-## vim aliases
+## vim
 alias vimvimrc='vim $env/data/.vimrc'
 alias vimsetenv='vim $env/setenv.sh'
 alias vimgitignore='vim $env/data/.gitignore'
 alias vimrecord='vim $personal/.private/record.txt'
+alias vimdump='vim $personal/.private/dump.txt'
 alias vimalias='vim $env/data/alias.sh'
 
 # out-dated
