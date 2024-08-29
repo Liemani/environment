0acfcb519b6b185c6df192bfe1b95ea6183f0f65
Liemani <parkjh2768@gmail.com>
2024-08-29 10:00:31 +0900

    Set gitconfig.defaultBranch = main

--- setup.sh.d/common/setup_git.sh
+++ setup.sh.d/common/setup_git.sh
@@ -6,3 +6,4 @@ git config --global user.name $git_user_name
 git config --global user.email $git_user_email
 git config --global core.excludesFile $environment/data/.gitignore
 git config --global alias.pull "git pull --rebase"
+git config --global init.defaultBranch main
