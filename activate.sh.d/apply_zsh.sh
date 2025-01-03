a774d0387b347eb6895e20d4b9aa544334bc6c21
Liemani <parkjh2768@gmail.com>
2025-01-03 18:23:30 +0900

    Add hostname to prompt

--- activate.sh.d/apply_zsh.sh
+++ activate.sh.d/apply_zsh.sh
@@ -24,15 +24,17 @@ lmi_generate_dynamic_prompt() {
 
     if [ "$git_root" ]; then
         prompt_path=${PWD##$git_root}
-    elif [[ "$PWD" == "$ENVHOME"* ]]; then
-        prompt_path='ENV~'${PWD##$ENVHOME}
     elif [[ "$PWD" == "$HOME"* ]]; then
         prompt_path='~'${PWD##$HOME}
+    elif [[ "$PWD" == "$ENVHOME"* ]]; then
+        prompt_path='ENV~'${PWD##$ENVHOME}
     else
         prompt_path=$PWD
     fi
 
-    echo -e %F{240}$(date "+%H:%M") %F{208}$git_repo$prompt_path %F{246}\(%F{34}$git_repo%F{246}/%F{34}$git_branch%F{246}\) %F{111}%# %f
+    hostname=`hostname -s`
+
+    echo -e %F{240}$(date "+%H:%M") %F{111}$hostname %F{208}$git_repo$prompt_path %F{246}\(%F{34}$git_repo%F{246}/%F{34}$git_branch%F{246}\) %F{111}%# %f
 
     unset prompt_path
 }
